a156502e3c281af5c3ed45d0f608fb674f438ed4
alexalexfg <alfaga88@gmail.com>
2023-12-17 22:01:00 +0100

    Actualizacion test_persistencia

--- test_persistencia.py
+++ test_persistencia.py
@@ -1,18 +1,16 @@
-from persistencia import  guardar_pedido
+"""Pruebas persistencia"""
+import  persistencia
 
+def test_guardar_pedido():
+    """Prueba general"""
 
-
-with open("pedidos.txt", "w", encoding="utf-8") as file:
-    file.write("")
-    file.close()
-
-
-pedidos = [{"nombre: ": "Pedro", "apellidos: ": "Gil de Diego"},
-           {"nombre: ": "Michael", "apellidos: ": "Jordan"}]       
-
-
-for elem in pedidos:      
-    for k,v in elem.items():       
-        guardar_pedido(k, v)    
+    with open("pedidos.txt", "w+", encoding="utf-8") as file:
+        persistencia.guardar_pedido("Pedro", "Gil de Diego")
+        persistencia.guardar_pedido("Michael", "Jordan")
+        firstline = file.readline()
+        secondline = file.readline()
+        file.close()
+        assert firstline == "-Pedro Gil de Diego\n"
+        assert secondline == "-Michael Jordan\n"  
 
 
