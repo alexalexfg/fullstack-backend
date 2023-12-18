b4bc1182fb78d06d65fca83e73fd29ed0f11ba67
alexalexfg <alfaga88@gmail.com>
2023-12-18 10:59:56 +0100

    Correcciones7

--- persistencia.py
+++ persistencia.py
@@ -5,7 +5,7 @@
 def guardar_pedido (nombre, apellido):
     """Función para guardar los pedidos en el archivo txt"""
     with open("pedidos.txt", "a", encoding="utf-8") as file:
-        file.write(-nombre + " " + apellido + "\n")
+        file.write("-"+ nombre + " " + apellido + "\n")
         file.close()
 
-guardar_pedido("-Gabriel","García")
+guardar_pedido("Gabriel","García")
